7bbb8840385a9d7547b1418ee105025187a0f19a
Jon Finn Angelo <jon@finnangelo.com>
2024-03-16 22:44:05 +1300

    v0.0.2 queryexplus - get Resource icon/images working again from net upgrade known issue

--- src/QueryExPlus/Gui/AboutForm.Designer.cs
+++ src/QueryExPlus/Gui/AboutForm.Designer.cs
@@ -68,7 +68,7 @@ namespace QueryExPlus
             // logoPictureBox
             // 
             this.logoPictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.logoPictureBox.Image = ((System.Drawing.Image)(resources.GetObject("logoPictureBox.Image")));
+            this.logoPictureBox.Image = QueryExPlus.Properties.Resource.logoPictureBox_Image;
             this.logoPictureBox.Location = new System.Drawing.Point(3, 3);
             this.logoPictureBox.Name = "logoPictureBox";
             this.tableLayoutPanel.SetRowSpan(this.logoPictureBox, 6);
